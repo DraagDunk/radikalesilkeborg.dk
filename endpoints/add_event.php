1e4fdc78e2030e8efe6ac9b6dda4efee03ed0d9c
Jesper Dam <jesper.dk@hotmail.dk>
2025-12-03 22:26:42 +0100

    Ryd op i events.json når et nyt arrangement tilføjes: Sortering og sletning af gamle arrangementer

--- endpoints/add_event.php
+++ endpoints/add_event.php
@@ -3,6 +3,25 @@
 
     $pass_val = getenv("ADD_EVENT_PASSWORD");
 
+    function cleanupEvents($events) {
+        $curr_time = time();
+        foreach ($events as $key => $value) {
+            if (getTimestamp($value) < $curr_time) {
+                unset($events[$key]);
+            }
+        }
+        $time_col = array_column($events, "time");
+        array_multisort($time_col, SORT_ASC, $events);
+
+        return $events;
+    }
+
+    function getTimestamp($event) {
+        $time = date_parse($event["time"]);
+        $timestamp = mktime($time["hour"], $time["minute"], $time["second"], $time["month"], $time["day"], $time["year"]);
+        return $timestamp;
+    }
+
     if ($password == $pass_val) {
         $postArray = array(
             "name"=> $_POST["name"] ?? "",
@@ -12,9 +31,11 @@
 
         $file = "../events.json";
 
-        $existing = json_decode(file_get_contents($file));
+        $existing = json_decode(file_get_contents($file), true);
 
         $existing[] = $postArray;
+
+        $existing = cleanupEvents($existing);
 
         $json = json_encode($existing);
 
