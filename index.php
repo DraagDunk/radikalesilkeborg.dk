745600f80df62294061d314ab1494c04a53bacc4
Jesper <jesper.dk@hotmail.dk>
2026-01-08 20:27:00 +0100

    Opdater bestyrelsen med nye poster

--- index.php
+++ index.php
@@ -1,217 +1,225 @@
 <?php
-    $title = "Forside";
-    include("header.php");
+$title = "Forside";
+include("header.php");
 ?>
+
 <body>
 
-<?php
-    include("navigation.php");
-?>
-
-<div class="page">
-    <div class="content">
-
     <?php
-        $json = file_get_contents("./events.json");
-        $arr = json_decode($json, true);
-
-        if ($arr)
-            include("events/index.php");
+    include("navigation.php");
     ?>
 
+    <div class="page">
+        <div class="content">
 
-        <h2 id="politik">Politisk program</h2>
+            <?php
+            $json = file_get_contents("./events.json");
+            $arr = json_decode($json, true);
 
-    <div class="policy-grid">
+            if ($arr)
+                include("events/index.php");
+            ?>
 
-        <div class="policy">
-            <h3 id="born_og_unge">Børn og unge</h3>
 
-            <p>
-                Børn, unge og uddannelse er en hjertesag for Radikale Venstre. Al radikal politik i Silkeborg kommune tager udgangspunkt i vores <a href="https://www.radikale.dk/politik/vi-mener/udspil/oekonomi/2030-plan-vi-vil-lave-en-ny-generations-kontrakt/" target="_blank">generationskontrakt</a>. Derfor skal økonomien i skolerne og dagtilbudene i kommunen forbedres, så vi kan leve op til vores ambitioner om uddannelsestilbud af højeste kvalitet.
-            </p>
-        </div>
+            <h2 id="politik">Politisk program</h2>
 
-        <div class="policy">
-            <h3 id="klima_og_miljo">Klima og miljø</h3>
-            
-            <p>
-                Meget simpelt kan Radikale Venstres klima- og miljøpolitik koges ned til: Vi ønsker mindre CO2 i atmosfæren og højere biodiversitet i naturen. Vi støtter de initiativer, som er igangsat af Silkeborg kommune med henblik på at leve op til FNs og Folketingets målsætninger.
-            </p>
-        </div>
+            <div class="policy-grid">
 
-        <div class="policy">
-            <h3 id="kultur">Kultur, fritid og outdoor</h3>
+                <div class="policy">
+                    <h3 id="born_og_unge">Børn og unge</h3>
 
-            <p>
-                Flere borgere i Silkeborg skal have mulighed for at kunne deltage i kommunens kultur-, fritids-, outdoor- og idrætsliv. Vi ønsker at støtte op om det inkluderende og favnende foreningsliv, som alle kan være en del af og høre med til. Samtidigt ønsker vi at foreningslivet i høj grad skal inddrage søhøjlandets enestående natur.
-            </p>
-        </div>
+                    <p>
+                        Børn, unge og uddannelse er en hjertesag for Radikale Venstre. Al radikal politik i Silkeborg
+                        kommune tager udgangspunkt i vores <a
+                            href="https://www.radikale.dk/politik/vi-mener/udspil/oekonomi/2030-plan-vi-vil-lave-en-ny-generations-kontrakt/"
+                            target="_blank">generationskontrakt</a>. Derfor skal økonomien i skolerne og dagtilbudene i
+                        kommunen forbedres, så vi kan leve op til vores ambitioner om uddannelsestilbud af højeste
+                        kvalitet.
+                    </p>
+                </div>
 
-        <div class="policy">
-            <h3 id="social">Socialpolitik</h3>
+                <div class="policy">
+                    <h3 id="klima_og_miljo">Klima og miljø</h3>
 
-            <p>
-                Den økonomiske ulighed er stigende, og dette opfatter vi i Radikale Venstre som en trussel mod friheden i samfundet. Som borgere skal vi kunne stole på, at et net griber os, hvis vi er syge eller udsatte. Samtidigt fortjener alle at blive mødt med klare forventninger. Derfor er det vores målsætning, at så mange som muligt er en del af arbejdsmarkedet, i den udstrækning det er muligt.
-            </p>
-        </div>
+                    <p>
+                        Meget simpelt kan Radikale Venstres klima- og miljøpolitik koges ned til: Vi ønsker mindre CO2 i
+                        atmosfæren og højere biodiversitet i naturen. Vi støtter de initiativer, som er igangsat af
+                        Silkeborg kommune med henblik på at leve op til FNs og Folketingets målsætninger.
+                    </p>
+                </div>
 
-        <div class="policy">
-            <h3 id="sundhed">Sundhed</h3>
+                <div class="policy">
+                    <h3 id="kultur">Kultur, fritid og outdoor</h3>
 
-            <p>
-                Med rod i den frie og lige adgang til det danske sundhedsvæsen, ønsker vi i Radikale Venstre at øge sundheden ved forebyggelse og nærmere sundhedstilbud. Forebyggelse skal især have fokus på de væsentligste årsager til dårligt helbred: Ensomhed og social ulighed, alkohol, rygning og svær overvægt. Derudover ønsker vi en døgnbemandet skadestue på sygehuset i Silkeborg, samt at bevare bloddonorklinikken og den psykiatriske skadestue.
-            </p>
-        </div>
+                    <p>
+                        Flere borgere i Silkeborg skal have mulighed for at kunne deltage i kommunens kultur-, fritids-,
+                        outdoor- og idrætsliv. Vi ønsker at støtte op om det inkluderende og favnende foreningsliv, som
+                        alle kan være en del af og høre med til. Samtidigt ønsker vi at foreningslivet i høj grad skal
+                        inddrage søhøjlandets enestående natur.
+                    </p>
+                </div>
 
-        <div class="policy">
-            <h3 id="aeldre">Ældre</h3>
+                <div class="policy">
+                    <h3 id="social">Socialpolitik</h3>
 
-            <p>
-                Den voksende andel af ældre borgere i kommunen kan ikke understøttes af penge alene. Det er nødvendigt at indføre tiltag, som kan forbedre arbejdsvilkårerne på plejehjemmene og i ældreplejen, så der bliver mere tid til det der betyder noget: Pleje. Dette betyder i høj grad større indflydelse blandt plejepersonalet i tilrettelæggelse af egne arbejdsgange.
-            </p>
-        </div>
+                    <p>
+                        Den økonomiske ulighed er stigende, og dette opfatter vi i Radikale Venstre som en trussel mod
+                        friheden i samfundet. Som borgere skal vi kunne stole på, at et net griber os, hvis vi er syge
+                        eller udsatte. Samtidigt fortjener alle at blive mødt med klare forventninger. Derfor er det
+                        vores målsætning, at så mange som muligt er en del af arbejdsmarkedet, i den udstrækning det er
+                        muligt.
+                    </p>
+                </div>
 
-        <div class="policy">
-            <h3 id="trafik">Plan og vej</h3>
+                <div class="policy">
+                    <h3 id="sundhed">Sundhed</h3>
 
-            <p>
-                Silkeborg er en kommune i udvikling, og det er derfor vigtigt at træffe de rigtige, bæredygtige beslutninger tidligt, så vi kan understøtte en stigende befolkning. Byfortætningen i Silkeborg skal ske med respekt for ældre byggeri, og sikre åbne arealer. Derudover skal trafikhierarkiet vendes, så de bløde trafikanter indtænkes før bilisme. Den offentlig transport skal støttes i højere grad, og forbindelserne skal understøtte en hverdag uden en bil.
-            </p>
-        </div>
-        </div>
+                    <p>
+                        Med rod i den frie og lige adgang til det danske sundhedsvæsen, ønsker vi i Radikale Venstre at
+                        øge sundheden ved forebyggelse og nærmere sundhedstilbud. Forebyggelse skal især have fokus på
+                        de væsentligste årsager til dårligt helbred: Ensomhed og social ulighed, alkohol, rygning og
+                        svær overvægt. Derudover ønsker vi en døgnbemandet skadestue på sygehuset i Silkeborg, samt at
+                        bevare bloddonorklinikken og den psykiatriske skadestue.
+                    </p>
+                </div>
 
-        <h2 id="best">Bestyrelsen</h2>
+                <div class="policy">
+                    <h3 id="aeldre">Ældre</h3>
 
-        <div class="person">
-            <div class="image">
-                <img src="jesper-knudgaard/Foto_JesperDamKnudgaard.jpg" alt="">
-            </div>
-            <div class="info">
-                <h3>Jesper Dam Gynther Knudgaard</h3>
-                <div class="post">Formand</div>
-                <div class="contact-info">
-                <a href="mailto:jesper@radikalesilkeborg.dk" class="sm-icon fa fa-envelope"><span class="icon-text">jesper@radikalesilkeborg.dk</span></a>
+                    <p>
+                        Den voksende andel af ældre borgere i kommunen kan ikke understøttes af penge alene. Det er
+                        nødvendigt at indføre tiltag, som kan forbedre arbejdsvilkårerne på plejehjemmene og i
+                        ældreplejen, så der bliver mere tid til det der betyder noget: Pleje. Dette betyder i høj grad
+                        større indflydelse blandt plejepersonalet i tilrettelæggelse af egne arbejdsgange.
+                    </p>
+                </div>
+
+                <div class="policy">
+                    <h3 id="trafik">Plan og vej</h3>
+
+                    <p>
+                        Silkeborg er en kommune i udvikling, og det er derfor vigtigt at træffe de rigtige, bæredygtige
+                        beslutninger tidligt, så vi kan understøtte en stigende befolkning. Byfortætningen i Silkeborg
+                        skal ske med respekt for ældre byggeri, og sikre åbne arealer. Derudover skal trafikhierarkiet
+                        vendes, så de bløde trafikanter indtænkes før bilisme. Den offentlig transport skal støttes i
+                        højere grad, og forbindelserne skal understøtte en hverdag uden en bil.
+                    </p>
                 </div>
             </div>
-        </div>
 
-        <div class="person">
-            <div class="image">
-                <img src="noone.png" alt="">
-            </div>
-            <div class="info">
-                <h3>Per Simon Knudsen</h3>
-                <div class="post">Kasserer</div>
-            </div>
-        </div>
+            <h2 id="best">Bestyrelsen</h2>
 
-        <div class="person">
-            <div class="image">
-                <img src="carsten-bjerre/CarstenBjerre-portrait.jpg" alt="">
+            <div class="person">
+                <div class="image">
+                    <img src="jesper-knudgaard/Foto_JesperDamKnudgaard.jpg" alt="">
+                </div>
+                <div class="info">
+                    <h3>Jesper Dam Gynther Knudgaard</h3>
+                    <div class="post">Formand</div>
+                    <div class="contact-info">
+                        <a href="mailto:jesper@radikalesilkeborg.dk" class="sm-icon fa fa-envelope"><span
+                                class="icon-text">jesper@radikalesilkeborg.dk</span></a>
+                    </div>
+                </div>
             </div>
-            <div class="info">
-                <h3>Carsten Bjerre</h3>
-                <div class="post">Bestyrelsesmedlem</div>
-            </div>
-        </div>
 
-        <div class="person">
-            <div class="image">
-                <img src="noone.png" alt="">
+            <div class="person">
+                <div class="image">
+                    <img src="noone.png" alt="">
+                </div>
+                <div class="info">
+                    <h3>Per Simon Knudsen</h3>
+                    <div class="post">Kasserer</div>
+                </div>
             </div>
-            <div class="info">
-                <h3>Henrik Steffens</h3>
-                <div class="post">Bestyrelsesmedlem</div>
-            </div>
-        </div>
 
-        <div class="person">
-            <div class="image">
-                <img src="noone.png" alt="">
+            <div class="person">
+                <div class="image">
+                    <img src="/carsten-skytt/foto.jpeg" alt="">
+                </div>
+                <div class="info">
+                    <h3>Carsten Ørum Skytt</h3>
+                    <div class="post">Sekretær</div>
+                </div>
             </div>
-            <div class="info">
-                <h3>Klara Gynther Knudgaard</h3>
-                <div class="post">Bestyrelsesmedlem</div>
-            </div>
-        </div>
 
-        <div class="person">
-            <div class="image">
-                <img src="/morten-junget/foto.jpeg" alt="">
+            <div class="person">
+                <div class="image">
+                    <img src="noone.png" alt="">
+                </div>
+                <div class="info">
+                    <h3>Klara Gynther Knudgaard</h3>
+                    <div class="post">Næstforperson</div>
+                </div>
             </div>
-            <div class="info">
-                <h3>Morten Junget</h3>
-                <div class="post">Bestyrelsesmedlem</div>
-            </div>
-        </div>
 
-        <div class="person">
-            <div class="image">
-                <img src="noone.png" alt="">
+            <div class="person">
+                <div class="image">
+                    <img src="carsten-bjerre/CarstenBjerre-portrait.jpg" alt="">
+                </div>
+                <div class="info">
+                    <h3>Carsten Bjerre</h3>
+                    <div class="post">Bestyrelsesmedlem</div>
+                </div>
             </div>
-            <div class="info">
-                <h3>Poul Elmegaard</h3>
-                <div class="post">Bestyrelsesmedlem</div>
-            </div>
-        </div>
 
-        <div class="person">
-            <div class="image">
-                <img src="/kim-egeskov/foto.jpeg" alt="">
+            <div class="person">
+                <div class="image">
+                    <img src="/morten-junget/foto.jpeg" alt="">
+                </div>
+                <div class="info">
+                    <h3>Morten Junget</h3>
+                    <div class="post">Bestyrelsesmedlem</div>
+                </div>
             </div>
-            <div class="info">
-                <h3>Kim Egeskov</h3>
-                <div class="post">Bestyrelsesmedlem</div>
-            </div>
-        </div>
 
-        <div class="person">
-            <div class="image">
-                <img src="/carsten-skytt/foto.jpeg" alt="">
+            <div class="person">
+                <div class="image">
+                    <img src="noone.png" alt="">
+                </div>
+                <div class="info">
+                    <h3>Poul Elmegaard</h3>
+                    <div class="post">Bestyrelsesmedlem</div>
+                </div>
             </div>
-            <div class="info">
-                <h3>Carsten Ørum Skytt</h3>
-                <div class="post">Suppleant</div>
-            </div>
-        </div>
 
-        <div class="person">
-            <div class="image">
-                <img src="johan-broedsgaard/IMG_7521.jpeg" alt="">
+            <div class="person">
+                <div class="image">
+                    <img src="/kim-egeskov/foto.jpeg" alt="">
+                </div>
+                <div class="info">
+                    <h3>Kim Egeskov</h3>
+                    <div class="post">Bestyrelsesmedlem</div>
+                </div>
             </div>
-            <div class="info">
-                <h3>Johan Brødsgaard</h3>
-                <div class="post">Suppleant</div>
-            </div>
-        </div>
 
-        <div class="person">
-            <div class="image">
-                <img src="/søren-wenzel/foto.jpeg" alt="">
+            <div class="person">
+                <div class="image">
+                    <img src="johan-broedsgaard/IMG_7521.jpeg" alt="">
+                </div>
+                <div class="info">
+                    <h3>Johan Brødsgaard</h3>
+                    <div class="post">Suppleant</div>
+                </div>
             </div>
-            <div class="info">
-                <h3>Søren Kristian Wenzel Nielsen</h3>
-                <div class="post">Suppleant</div>
-            </div>
-        </div>
 
 
 
-        <!-- <div class="social-media-buttons">
+            <!-- <div class="social-media-buttons">
                     <a href="https://www.instagram.com/person_personsen_not_a_real_person" class="sm-icon fa-brands fa-instagram"></a>
                     <a href="https://www.x.com/person_personsen_not_a_real_person" class="sm-icon fa-brands fa-x-twitter"></a>
                     <a href="https://www.facebook.com/person_personsen_not_a_real_person" class="sm-icon fa-brands fa-facebook"></a>
                     <a href="https://www.tiktok.com/person_personsen_not_a_real_person" class="sm-icon fa-brands fa-tiktok"></a>
                     <a href="mailto:dennemailfindesheltsikkert@ikke.com" class="sm-icon fa fa-envelope"></a>
                 </div> -->
+        </div>
     </div>
-</div>
 
-<?php
+    <?php
     include("footer.php");
-?>
+    ?>
 
-    
+
 </body>
+
 </html>
