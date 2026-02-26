86dfc199ff761df1faf45a3e2687ef9f3e314c3a
Jesper <jesper.dk@hotmail.dk>
2026-02-26 20:40:45 +0100

    Tilføj side med almindelige spørgsmål (SEO)

---
+++ faq/index.php
@@ -0,0 +1,79 @@
+<?php
+$title = "Forside";
+include("../header.php");
+?>
+
+<body>
+
+    <?php
+    include("../navigation.php");
+    ?>
+
+    <div class="page">
+        <div class="content">
+            <h1>Spørgsmål og svar</h1>
+            <div class="policy-grid">
+                <div class="policy">
+                    <h3>Er der udskrevet folketingsvalg i Danmark i 2026?</h3>
+
+                    <p>
+                        Ja, statsminister Mette Frederiksen udskrev valg den 26. februar 2026. Valget skal afholdes den
+                        24.
+                        marts 2026. Til valget opstiller mange politiske partier, blandt andre Radikale Venstre.
+                    </p>
+                </div>
+
+                <div class="policy">
+                    <h3>Hvornår er der folketingsvalg i Danmark i 2026?</h3>
+
+                    <p>
+                        Det danske folketingsvalg i 2026 afholdes den 24. marts. Her opstiller mange politiske partier,
+                        blandt andre Radikale Venstre.
+                    </p>
+                </div>
+
+                <div class="policy">
+                    <h3>Hvornår kan man brevstemme til folketingsvalget 2026?</h3>
+
+                    <p>
+                        Du kan brevstemme til folketingsvalget allerede nu, og helt frem til den 20. marts 2026. Når du
+                        brevstemmer kan du stemme på alle opstillede politiske partier og kandidater, blandt andre
+                        Radikale Venstre. Du brevstemmer på <a href="https://borger.dk">borger.dk</a>.
+                    </p>
+                </div>
+
+                <div class="policy">
+                    <h3>Hvor finder jeg kandidattest/valgtest til folketingsvalget 2026?</h3>
+
+                    <p>
+                        Der er endnu ingen tilgængelige valgtest til folketingsvalget 2026. Når valgtest fra f. eks. DR
+                        og Altinget bliver tilgængelige, kan man læse om politiske holdninger fra alle opstillede
+                        kandidater, blandt andre dem, der er opstillede for Radikale Venstre.
+                    </p>
+                </div>
+
+                <div class="policy">
+                    <h3>Hvad er folketingsvalget 2026?</h3>
+
+                    <p>
+                        Folketingsvalget 2026 er valget til Danmarks højeste lovgivende forsamling: Folketinget. Her
+                        skal vælges 179 repræsentanter. Kandidater til folketinget opstilles oftes på vegne af politiske
+                        partier, blandt andre Radikale Venstre.
+                    </p>
+                </div>
+
+                <div class="policy">
+                    <h3>Hvem skal jeg stemme på til folketingsvalget 2026?</h3>
+
+                    <p>
+                        Til folketingsvalget skal du stemme på den folketingskandidat, som du er mest enig med. Er du
+                        for eksempel mest enig i den politik, som Radikale Venstre fører, er det en god ide, at stemme
+                        på en kandidat fra Radikale Venstre.
+                    </p>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+</body>
